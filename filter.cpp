65b7c735a420eabd7c515fe7f8e01657856071ac
Tim Haering <tim.haering@gmail.com>
2020-07-08 14:53:31 +0200

    add makefile

--- filter.cpp
+++ filter.cpp
@@ -1,17 +1,15 @@
 #include "filter.h"
 
-void gauss_simple (uint8_t video_in[ROWS*COLS], uint8_t video_out[ROWS*COLS]) {
-#pragma HLS INTERFACE m_axi port=video_in bundle=gmem0
-#pragma HLS INTERFACE axis port=video_out bundle=out_stream
+void gauss_simple(uint8_t video_in[ROWS * COLS],
+                  uint8_t video_out[ROWS * COLS]) {
+#pragma HLS INTERFACE m_axi port = video_in bundle = gmem0
+#pragma HLS INTERFACE axis port = video_out bundle = out_stream
 
-    int16_t kernel[KERN_SIZE][KERN_SIZE] = {
-        {1, 2, 1},
-        {2, 4, 2},
-        {1, 2, 1}};
-#pragma HLS array_partition variable=kernel complete dim=0
+    int16_t kernel[KERN_SIZE][KERN_SIZE] = {{1, 2, 1}, {2, 4, 2}, {1, 2, 1}};
+#pragma HLS array_partition variable = kernel complete dim = 0
 
-    for(int32_t y=0; y<ROWS; y++) {
-        for(int32_t x=0; x<COLS; x++) {
+    for (int32_t y = 0; y < ROWS; y++) {
+        for (int32_t x = 0; x < COLS; x++) {
 #pragma HLS pipeline
 
             int16_t sum = 0;
@@ -19,45 +17,14 @@ void gauss_simple (uint8_t video_in[ROWS*COLS], uint8_t video_out[ROWS*COLS]) {
                 for (int32_t j = -KERN_RAD; j <= KERN_RAD; j++) {
                     int32_t yi = y + i;
                     int32_t xj = x + j;
-                    if ((xj >= 0) && (xj < COLS) && (y + i >= 0) && (y + i < ROWS))
-                        sum += (int16_t)(video_in[yi * COLS + xj]) * kernel[i + KERN_RAD][j + KERN_RAD];
+                    if ((xj >= 0) && (xj < COLS) && (y + i >= 0) &&
+                        (y + i < ROWS))
+                        sum += (int16_t)(video_in[yi * COLS + xj]) *
+                               kernel[i + KERN_RAD][j + KERN_RAD];
                 }
             }
             // Normalize and store the result
             video_out[y * COLS + x] = (uint8_t)(MAX(MIN(255, sum >> 4), 0));
-        }
-    }
-}
-
-void sobel_simple (uint8_t video_in[ROWS*COLS], uint8_t video_out[ROWS*COLS]){
-#pragma HLS INTERFACE m_axi port=video_in bundle=gmem0
-#pragma HLS INTERFACE axis port=video_out bundle=out_stream
-
-    int16_t kernel[KERN_SIZE][KERN_SIZE] = {
-        {1, 0, -1},
-        {2, 0, -2},
-        {1, 0, -1}};
-#pragma HLS array_partition variable=kernel complete dim=0
-
-    for(int32_t y=0; y<ROWS; y++) {
-        for(int32_t x=0; x<COLS; x++) {
-#pragma HLS pipeline
-
-            int16_t sum = 0;
-            for (int32_t i = -KERN_RAD; i <= KERN_RAD; i++) {
-                for (int32_t j = -KERN_RAD; j <= KERN_RAD; j++) {
-                    int32_t yi = y + i;
-                    int32_t xj = x + j;
-                    if ((xj >= 0) && (xj < COLS) && (y + i >= 0) && (y + i < ROWS))
-                        sum += (int16_t)(video_in[yi * COLS + xj]) * kernel[i + KERN_RAD][j + KERN_RAD];
-                }
-            }
-
-            // Normalize and store the result
-            //sum = MAX(MIN(255, (sum >> 3) + 128), 0);
-            sum = abs(sum >> 3) << 1;
-
-            video_out[y * COLS + x] = (uint8_t)(sum);
         }
     }
 }
