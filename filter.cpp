83bb560f7976208793a4bf6414838489c92d6843
Tim Haering <tim.haering@gmail.com>
2020-07-08 10:59:06 +0200

    implement linebuffer

--- filter.cpp
+++ filter.cpp
@@ -4,61 +4,87 @@ void gauss_simple (uint8_t video_in[ROWS*COLS], uint8_t video_out[ROWS*COLS]) {
 #pragma HLS INTERFACE axis port=video_in bundle=in_stream
 #pragma HLS INTERFACE axis port=video_out bundle=out_stream
 
-    int16_t kernel[KERN_SIZE][KERN_SIZE] = {
+    uint16_t kernel[KERN_SIZE][KERN_SIZE] = {
         {1, 2, 1},
         {2, 4, 2},
         {1, 2, 1}};
+    uint8_t line_buf[KERN_SIZE - 1][COLS] = { 0 };
+    uint8_t right_side[KERN_SIZE] = { 0 };
+    uint8_t window[KERN_SIZE][KERN_SIZE] = { 0 };
 #pragma HLS array_partition variable=kernel complete dim=0
+#pragma HLS array_partition variable=line_buf complete dim=1
+#pragma HLS array_partition variable=right_side complete
+#pragma HLS array_partition variable=window complete dim=0
 
-    uint8_t row0[COLS+2] = { 0 },
-            row1[COLS+2] = { 0 };
-    uint8_t window[3][3] = { 0 };
+    uint32_t counter_stream = 0;
 
-    int counter_stream = 0;
-    int16_t sum = 0;
-
-    // read first image row into second buffer
-    for (int var = 0; var < COLS; var++) {
-        row1[var+1] = video_in[var];
+    // Read first image rows into line buffer
+    for (int x = COLS - KERN_RAD - 1; x < COLS; x++) {
+#pragma HLS pipeline
+        line_buf[KERN_RAD - 1][x] = video_in[counter_stream];
+        counter_stream++;
     }
+    for (int y = KERN_RAD; y < KERN_SIZE - 1; ++y) {
+		for (int x = 0; x < COLS; ++x) {
+#pragma HLS pipeline
+			line_buf[y][x] = video_in[counter_stream];
+			counter_stream++;
+		}
+	}
 
-    // iterate over all image pixels
-    for(int32_t i=0; i<(ROWS*COLS); i++) {
+    // Populate window
+    for (int y = KERN_RAD; y < KERN_SIZE; ++y) {
+    	for (int x = KERN_RAD; x < KERN_SIZE; ++x) {
+#pragma HLS pipeline
+			window[y][x] = line_buf[y - 1][x + COLS - KERN_SIZE];
+		}
+	}
+
+    // Iterate over all image pixels
+    for (int y = 0; y < ROWS; ++y) {
+		for (int x = 0; x < COLS; ++x) {
 #pragma HLS pipeline
 
-        // shift pixels in sliding window
-        for (int k = 0; k < 3; k++) {
-            for (int j = 0; j < 2; j++) {
-                window[k][j] = window[k][j+1];
-            }
-        }
-        window[0][2] = row0[counter_stream];
-        window[1][2] = row1[counter_stream];
-        if (i >= (ROWS-1)*(COLS+2)) {
-            window[2][2] = 0;
-        } else {
-            if (!(i % (COLS+2) || i % (COLS+1))) {
-                window[2][2] = 0;
-            } else {
-                window[2][2] = video_in[i+COLS];
-            }
-        }
+	        // Calculate resulting pixel
+		    uint16_t sum = 0;
+	        for (int i = -KERN_RAD; i <= KERN_RAD; i++) {
+	            for (int j = -KERN_RAD; j <= KERN_RAD; j++) {
+                    uint8_t yi = y + i;
+                    uint8_t xj = x + j;
+	            	if (0 <= yi && yi < ROWS && 0 <= xj && xj < COLS) {
+	            		sum += (uint16_t)((window[i + KERN_RAD][j + KERN_RAD]) * kernel[i + KERN_RAD][j + KERN_RAD]);
+					}
+	            }
+	        }
 
-        // calculate resulting pixel
-        sum = 0;
-        for (int row = 0; row < 3; row++) {
-            for (int column = 0; column < 3; column++) {
-                sum += (int16_t)(window[row][column]) * kernel[row][column];
-            }
-        }
+	        // Normalize and store the result
+	        video_out[y * COLS + x] = (uint8_t)(MAX(MIN(255, sum >> 4), 0));
 
-        // Normalize and store the result
-        video_out[i] = (uint8_t)(MAX(MIN(255, sum >> 4), 0));
+	        // Shift buffer
+	        right_side[0] = line_buf[0][x];
+	        for (int y = 1; y < KERN_SIZE - 1; ++y) {
+				right_side[y] = line_buf[y - 1][x] = line_buf[y][x];
+			}
 
-        // shift buffers and pointer
-        row0[counter_stream] = row1[counter_stream];
-        row1[counter_stream] = window[2][2];
-        counter_stream++;
-        counter_stream = counter_stream % (COLS-1);
-    }
+	        // Read new value
+	        uint8_t val_in = 0;
+			if (counter_stream < ROWS * COLS) {
+				val_in = video_in[counter_stream];
+				counter_stream++;
+			}
+			right_side[KERN_SIZE - 1] = line_buf[KERN_SIZE - 2][x] = val_in;
+
+			// Shift window content
+			for (int y = 0; y < KERN_SIZE; y++) {
+				for (int x = 0; x < KERN_SIZE - 1; x++) {
+					window[y][x] = window[y][x + 1];
+				}
+			}
+
+		    // Update window values at right side
+			for (int y = 0; y < KERN_SIZE; y++) {
+				window[y][KERN_SIZE - 1] = right_side[y];
+			}
+		}
+	}
 }
--- filter.h
+++ filter.h
@@ -8,16 +8,14 @@
 #include <ctype.h>
 #include <algorithm>
 
-#define COLS 850
-#define ROWS 680
+#define COLS 512
+#define ROWS 512
 #define KERN_SIZE 3
 #define KERN_RAD (KERN_SIZE >> 1)
 
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
-
-void sobel_simple (uint8_t video_in[ROWS*COLS], uint8_t video_out[ROWS*COLS]);
 void gauss_simple (uint8_t video_in[ROWS*COLS], uint8_t video_out[ROWS*COLS]);
 
 #endif
